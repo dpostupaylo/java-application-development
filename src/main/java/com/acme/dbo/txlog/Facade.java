97319687444c3c3cf4600f3f466f6e33f759efcb
Dmitry Postupaylo <d.postupaylo@yandex.ru>
2022-04-12 11:22:27 +0300

    autotests

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -8,4 +8,24 @@ public class Facade {
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        System.out.println("primitive: " + message);
+    }
+
+    public static void main(String[] args) {
+        Facade.log(new Object());
+    }
+
+    public static void log(Object message) {
+        System.out.println("reference: " + message.toString());
+    }
 }
