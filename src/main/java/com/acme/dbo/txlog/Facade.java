59869c6c1671a25ce0002df4e8fa6024c63c3afa
Dmitry Postupaylo <d.postupaylo@yandex.ru>
2022-04-21 09:25:53 +0300

    autotests

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,17 +1,48 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    public static void log(int message) { MessagePrinter.printMessage(MessageDecorator.decorate(message)); }
+    private static int currentInt = 0;
+    private static byte currentByte = 0;
+    private static String currentString = "";
+
+    private static int countString = 0;
+    private static Object previousMessage = new Object();
+
+    public static void log(int message) {
+        if (previousMessage.getClass() != int.class) {
+            flush();
+            previousMessage = message;
+        }
+
+        currentInt += message;
+    }
 
     public static void log(byte message) {
-        MessagePrinter.printMessage(MessageDecorator.decorate(message));
+        if (previousMessage.getClass() != byte.class) {
+            flush();
+            previousMessage = message;
+        }
+
+        currentByte += message;
     }
 
     public static void log(char message) {
         MessagePrinter.printMessage(MessageDecorator.decorate(message));
     }
 
-    public static void log(String message) { MessagePrinter.printMessage(MessageDecorator.decorate(message)); }
+    public static void log(String message) {
+        if (previousMessage.getClass() != String.class) {
+            flush();
+            previousMessage = message;
+        }
+
+        if (currentString.equals(message) || countString == 0) {
+            countString += 1;
+            currentString = message;
+        } else {
+            flush();
+        }
+    }
 
     public static void log(boolean message) {
         MessagePrinter.printMessage(MessageDecorator.decorate(message));
@@ -19,5 +50,19 @@ public class Facade {
 
     public static void log(Object message) {
         MessagePrinter.printMessage(MessageDecorator.decorate(message));
+    }
+
+    public static void flush() {
+        if (currentInt > 0)
+            MessagePrinter.printMessage(MessageDecorator.decorate(currentInt));
+        if (currentByte > 0)
+            MessagePrinter.printMessage(MessageDecorator.decorate(currentByte));
+        if (!currentString.equals("") && countString != 0) {
+            MessagePrinter.printMessage(MessageDecorator.decorate(currentString));
+        }
+        countString = 0;
+        currentInt = 0;
+        currentByte = 0;
+        currentString = "";
     }
 }
